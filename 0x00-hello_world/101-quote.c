bcbe1007f5eb1108b96d6163cfd999b8b58a11af
Shad330 <shady.anany330@gmail.com>
2023-03-16 10:41:19 -0700

    task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print string into standrd error
+ *
+ * Return: 1 (Fails)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
