2d8d7b69f96e37778f8f45ac33ceb4e292f227dd
Shad330 <shady.anany330@gmail.com>
2023-03-17 17:17:39 -0700

    task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  C program to demonstrate putchar() method
+ *
+ * Return: Always o (success)
+*/
+
+int main(void)
+{
+char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+{
+	putchar(ch);
+}
+	putchar('\n');
+
+	return (0);
+}
