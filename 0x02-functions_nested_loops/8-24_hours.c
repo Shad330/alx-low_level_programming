cc4c14a31bd3d7b5792b70297dfb671bb89a3429
Shad330 <shady.anany330@gmail.com>
2023-03-21 15:13:52 -0700

    11

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,14 @@ void jack_bauer(void)
 
 	for (hour = 0 ; hour <= 23 ; hour++)
 	{
+		for (min = 0; min <= 59 ; min++)
+		(
 		_putchar((hour / 10) + '0');
 		_putchar((hour % 10) + '0');
 		_putchar(':');
 		_putchar((min / 10) + '0');
 		_putchar((min % 10) + '0');
 		_putchar('\n');
+		}
 	}
 }
