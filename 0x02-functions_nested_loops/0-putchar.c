25f2c1c79ab2fe21e0bb3c6c6ace3336dee47164
Shad330 <shady.anany330@gmail.com>
2023-03-21 05:26:17 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ * main - print a long string
+ *
+ * Return: 0 always (success)
+*/
+
+int main(void)
+{
+
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 't';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar('\n');
+
+return (0);
+}
