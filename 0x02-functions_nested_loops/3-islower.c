5fb02ac2cbe1672f261a59659fdcfab721b02eaf
Shad330 <shady.anany330@gmail.com>
2023-03-21 13:25:17 -0700

    4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,14 @@
 /**
  * int _islower - function that checkes lower case letters
  *
+ * @c: checks for function input
+ *
+ * Return: returns 1 if c is lowercase
+ * 	otherwise always 0 (success)
 */
 
 int _islower(int c)
-{	
+{
 	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
