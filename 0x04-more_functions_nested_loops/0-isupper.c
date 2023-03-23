0020553bc748d61a6ed86cd2072f41bccc7d5da2
Shad330 <shady.anany330@gmail.com>
2023-03-23 04:08:18 -0700

    1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include"main.h"
 
 /**
- * _islower - function that checkes lower case letters
+ * _isupper - function that checkes lower case letters
  * @c: checks for function input
  * Return: returns 1 if c is lowercase,else  always 0 (success)
 */
 
-int _islower(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
