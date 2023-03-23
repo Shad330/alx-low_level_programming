c2b23722b9dfc020b3fa934345950943f92391cc
Shad330 <shady.anany330@gmail.com>
2023-03-23 05:16:09 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers  - C program to demonstrate putchar() method
+*/
+
+void print_numbers(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	_putchar(digit);
+
+	_putchar('\n');
+
+}
