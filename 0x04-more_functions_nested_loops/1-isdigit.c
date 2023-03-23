e2567f86668e71b706f4acb457676cbc4d5e1ad9
Shad330 <shady.anany330@gmail.com>
2023-03-23 04:40:57 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,20 +6,21 @@
  *
  * @c: is an integer value
  *
- * Return: 1 or 0 (success):wq
+ * Return: 1 or 0 (success)
 */
 
 int _isdigit(int c)
 {
-	int c;
-
-	for (c = 0; c <= 9; c ++}
+	for (c = 0; c <= 9; c++)
+	{
 		if (isdigit(c))
-
+		{
 		return (1);
-
+		}
 		else
-
+		{
 		return (0);
+		}
+	}
 }
 
