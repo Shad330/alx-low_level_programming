36a6d1f85c13a9a0099c1f9518c7226a1e810548
Shad330 <shady.anany330@gmail.com>
2023-03-23 04:28:32 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include"main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - Checks for digits in a given entry
+ *
+ * @c: is an integer value
+ *
+ * Return: 1 or 0 (success):wq
+*/
+
+int _isdigit(int c)
+{
+	int c;
+
+	for (c = 0; c <= 9; c ++}
+		if (isdigit(c))
+
+		return (1);
+
+		else
+
+		return (0);
+}
+
