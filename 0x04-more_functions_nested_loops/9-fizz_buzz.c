a5aefa4b6a058af49e2affaf6f1949e7643a2e19
Shad330 <shady.anany330@gmail.com>
2023-03-23 11:15:56 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints fizz buzz on given condition
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 100; digit++)
+	{
+		if (digit % 3 == 0)
+			printf(" Fizz");
+
+		else if (digit % 5 == 0)
+			printf(" Buzz");
+
+		else if (digit % 5 == 0 && digit % 3 == 0)
+			printf(" FizzBuzz");
+		else
+			printf(" %d", digit);
+	}
+	printf("\n");
+	return (0);
+}
