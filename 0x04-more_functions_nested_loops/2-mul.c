0cb2c56a79aa097a149a29f9246e3737a4601d6e
Shad330 <shady.anany330@gmail.com>
2023-03-23 04:54:48 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include <ctype.h>
+
+/**
+ * mul - multiplies 2 integers
+ *
+ * @a: is an integer value
+ *
+ * @b: is an integer value
+ *
+*/
+
+mul(int a, int b)
+{
+	multiplyNum(a, b);
+}
