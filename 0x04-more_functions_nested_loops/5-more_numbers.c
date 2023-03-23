a05d0f84d3fd1258ec0163a97ebfbd0784b1a6c7
Shad330 <shady.anany330@gmail.com>
2023-03-23 08:20:55 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void)
 {
 	int line, digit, count;
 
-	for (line = 0; line <= 10; line++)
+	for (line = 1; line <= 10; line++)
 	{
-		for (count = '0'; count <= 14; count++)
+		for (count = 0; count <= 14; count++)
 		{
 			digit = count;
 			if (count > 9)
@@ -21,7 +21,7 @@ void more_numbers(void)
 				_putchar(1 + '0');
 				digit = count % 10;
 			}
-			_putchar(count + '0');
+			_putchar(digit + '0');
 		}
 		_putchar('\n');
 	}
