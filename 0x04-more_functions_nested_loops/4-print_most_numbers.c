8bf5a11ca2bc02463271b38d4f43eefe890ad89a
Shad330 <shady.anany330@gmail.com>
2023-03-23 05:35:51 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,12 +12,13 @@ void print_most_numbers(void)
 	for (digit = 0; digit <= 9; digit++)
 	{
 		if (digit != 2 || digit != 4)
-
+		{
 		_putchar(digit + '0');
-
+		}
 		else
-
+		{
 		_putchar('\n');
+		}
 	}
 
 }
