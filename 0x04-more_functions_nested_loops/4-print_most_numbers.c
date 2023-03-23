7c3c5e2215d10b6a4b38696977b8ce9a3412efe7
Shad330 <shady.anany330@gmail.com>
2023-03-23 05:32:14 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_most_numbers  - C program to demonstrate putchar() method
+ * Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		if (digit != 2 || digit != 4)
+
+		_putchar(digit + '0');
+
+		else
+
+		_putchar('\n');
+	}
+
+}
