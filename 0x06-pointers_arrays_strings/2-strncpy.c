cdbbe6ab1c3f92587cee3811fb12e8332a3a5d18
Shad330 <shady.anany330@gmail.com>
2023-03-29 14:57:34 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int  c1;
+	int c1;
 
-	for (c1 = 0; c1 = 0 && src[c1] != '\0'; c1++)
+	for (c1 = 0; c1 < n && src[c1] != '\0'; c1++)
 		dest[c1] = src[c1];
 
-	while (c1 > n)
+	while (c1 < n)
 	{
 		dest[c1] = '\0';
 		c1++;
