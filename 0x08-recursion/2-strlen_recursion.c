dc45ef163eb148d964eae12b391f84cf68336120
Shad330 <shady.anany330@gmail.com>
2023-04-05 13:07:21 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ *_strlen_recursion - function that prints a string using rcursion
+ * @s: a string
+ * Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
