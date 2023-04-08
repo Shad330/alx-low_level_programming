0fd6330bad3b22d93f93b772cbdddd934fe18570
Shad330 <shady.anany330@gmail.com>
2023-04-08 13:42:31 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,12 @@
  *main - function that return the number of arguments
  *@argc:command line counts
  *@argv:arry of strings with the size of argc
- *Return: always 0
+ *Return: int
 */
 
 int main(int argc, char const *argv[])
 {
+	(void)argv;
 	printf("%d\n", argc - 1);
 
 	return (0);
