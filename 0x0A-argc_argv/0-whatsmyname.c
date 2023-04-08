82c456d38c68ebcf136e2568e6447ead83f35a62
Shad330 <shady.anany330@gmail.com>
2023-04-08 13:28:18 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include"stdio.h"
+
+/**
+ *main - function that return the program name
+ *@argc:command line counts
+ *@argv:arry of strings with the size of argc
+ *Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
