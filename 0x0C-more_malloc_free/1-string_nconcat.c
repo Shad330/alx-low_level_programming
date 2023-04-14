e351b38b561125c96319f24b71b46da9f7f129d6
Shad330 <shady.anany330@gmail.com>
2023-04-13 19:59:35 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,8 +9,10 @@
  * Return: string of type char, or null in case it fails
 */
 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
 	char *str;
-	unsigned int index, index2, len1, len2;
+	unsigned int i, k, len1, len2;
 
 	if (s1 == 0)
 		s1 = "";
@@ -27,10 +29,10 @@
 	str = malloc(sizeof(char) * (len1 + n + 1));
 	if (str == 0)
 		return (0);
-	for (index = 0; index < len1; index++)
-		*(str + index) = *(s1 + index);
-	for (index2 = 0; index2 < n; index2++, index++)
-		*(str + index) = *(s2 + index2);
-	*(str + index) = '\0';
+	for (i = 0; i < len1; i++)
+		*(str + i) = *(s1 + i);
+	for (k = 0; k < n; k++, k++)
+		*(str + i) = *(s2 + k);
+	*(str + i) = '\0';
 	return (str);
 }
